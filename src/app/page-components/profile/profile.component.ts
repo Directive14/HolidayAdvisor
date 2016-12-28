83d1cd05144dc7b9c05e1cd54de6d67f975ccfc2
iliangogov <iliangogov@gmail.com>
2016-12-28 12:24:49 +0200

    profile.component.ts updated

--- src/app/page-components/profile/profile.component.ts
+++ src/app/page-components/profile/profile.component.ts
@@ -1,9 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../_models';
 
 @Component({
     templateUrl: './profile.component.html'
 })
-export class ProfileComponent{
-    @Input() currentUser : User = JSON.parse(localStorage.getItem('currentUser'));
+export class ProfileComponent {
+    @Input() currentUser: User = new User;
+
+    constructor(){
+
+    }
+    ngOnInit() {
+        let storageUser = JSON.parse(localStorage.getItem('currentUser')).user;
+        this.currentUser.username = storageUser.username;
+        this.currentUser.firstName = storageUser.firstName;
+        this.currentUser.lastName = storageUser.lastName;
+    }
 }
