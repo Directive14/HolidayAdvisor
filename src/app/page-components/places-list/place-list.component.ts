686229108af43527fcdc506c93b15c342e125249
TonyNikolov <fatalclaw@hotmail.com>
2017-01-02 14:00:19 +0200

    more animations added

--- src/app/page-components/places-list/place-list.component.ts
+++ src/app/page-components/places-list/place-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, transition, state, trigger, style, animate } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Place } from '../../_models';
@@ -8,6 +8,20 @@ import { StarRatingComponent } from '../star-rating';
 @Component({
     templateUrl: './place-list.component.html',
     styleUrls:  ['./place-list.component.css'],
+    host: {
+     '[@routeAnimation]': 'true',
+     '[style.display]': "'block'",
+   },
+    animations: [
+    trigger('routeAnimation', [
+      state('*', style({transform: 'translateX(0)', opacity: 1})),
+      transition('void => *', [
+        style({transform: 'translateY(-100%)', opacity: 0}),
+        animate(500)
+      ]),
+      transition('* => void', animate(500, style({transform: 'translateY(100%)', opacity: 0})))
+    ])
+  ]
 })
 export class PlaceListComponent implements OnInit {
     ratingClicked: number;
