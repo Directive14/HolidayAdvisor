7c3ddc4ddd170d0b28c6e6520c36e2850ecf9511
iliangogov <iliangogov@gmail.com>
2017-01-02 17:42:31 +0200

    not-loggedin.component.ts implemented it's showing when not logged in user try to see private routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,11 +11,12 @@ import { CreatePlaceComponent } from './page-components/create-place';
 import { CreatePartnerComponent } from './page-components/create-partner';
 import { PartnerListComponent } from './page-components/partners-list';
 import { PlaceSingleComponent } from './page-components/place-single';
-import { UsersListComponent} from  './page-components/users-list';
+import { UsersListComponent } from './page-components/users-list';
 import { OtherPersonProfileComponent } from './page-components/other-person-profile';
 import { UpdateProfileComponent } from './page-components/update-profile';
-import {FeedListComponent} from './page-components/feed-list';
+import { FeedListComponent } from './page-components/feed-list';
 import { NotFoundComponent } from './page-components/not-found';
+import { NotLoggedInComponent } from './page-components/not-loggedin';
 import { AuthGuard } from './_guards';
 
 const appRoutes: Routes = [
@@ -24,15 +25,16 @@ const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'notfound', component: NotFoundComponent },
+    { path: 'notloggedin', component: NotLoggedInComponent },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-    { path: 'users', component: UsersListComponent, canActivate: [AuthGuard]  },
+    { path: 'users', component: UsersListComponent, canActivate: [AuthGuard] },
     { path: 'profile/:username', component: OtherPersonProfileComponent, canActivate: [AuthGuard] },
     { path: 'update', component: UpdateProfileComponent, canActivate: [AuthGuard] },
-    { path: 'places', component: PlaceListComponent, canActivate: [AuthGuard]  },
-    { path: 'places/:name', component: PlaceSingleComponent, canActivate: [AuthGuard]  },
-    { path: 'createplace', component: CreatePlaceComponent, canActivate: [AuthGuard]  },
+    { path: 'places', component: PlaceListComponent, canActivate: [AuthGuard] },
+    { path: 'places/:name', component: PlaceSingleComponent, canActivate: [AuthGuard] },
+    { path: 'createplace', component: CreatePlaceComponent, canActivate: [AuthGuard] },
     { path: 'partners', component: PartnerListComponent },
-    { path: 'createPartner', component: CreatePartnerComponent, canActivate: [AuthGuard]  },
+    { path: 'createPartner', component: CreatePartnerComponent, canActivate: [AuthGuard] },
     { path: 'feed', component: FeedListComponent },
     // otherwise redirect to home
     { path: '**', redirectTo: 'notfound' },
